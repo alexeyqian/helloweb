27310222e5884e0f76b037310d21ddb98c21b283
AlexeyQian <alexeyqian@gmail.com>
2021-02-16 16:02:22 +0700

    add vapor leaf

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,14 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("hello2") { req -> EventLoopFuture<View> in
+        return req.view.render("hello", ["name": "Leaf"])
+    }
+    
+    app.get("hello3") { req -> EventLoopFuture<View> in
+        return req.view.render("hello", ["name": "Leaf2"])
+    }
+    
+
 }
